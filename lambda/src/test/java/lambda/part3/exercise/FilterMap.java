3fa85e684b5ab1f388e26e8204ee553b1c005ec8
nikolai_plokhoi <nikolai_plokhoi@epam.com>
2017-10-10 16:15:10 +0300

    Добавлен тест для проверки LazyCollectionHelper.

--- lambda/src/test/java/lambda/part3/exercise/FilterMap.java
+++ lambda/src/test/java/lambda/part3/exercise/FilterMap.java
@@ -1,13 +1,19 @@
 package lambda.part3.exercise;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import static org.junit.Assert.assertEquals;
+
 public class FilterMap {
 
     public static class Container<T, R> {
+
         private final Predicate<T> predicate;
         private final Function<T, R> function;
 
@@ -59,5 +65,18 @@ public class FilterMap {
             // TODO
             throw new UnsupportedOperationException();
         }
+    }
+
+    @Test
+    public void test() {
+        List<Integer> integers = Arrays.asList(1, 2, 100, 110, 200, 300, 500);
+
+        List<String> result = new LazyCollectionHelper<>(integers).filter(val -> val > 10)
+                                                                  .filter(val -> val < 400)
+                                                                  .map(Object::toString)
+                                                                  .filter(str -> str.startsWith("1"))
+                                                                  .force();
+
+        assertEquals(Arrays.asList("100", "110"), result);
     }
 }
