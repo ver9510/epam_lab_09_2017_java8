eee63a28bca2c496a26cad6b9db5fa0e79ec669e
Вера <ver9510@yandex.ru>
2017-10-17 14:14:15 +0300

    Done exercise 1

--- streams/src/test/java/part1/exercise/StreamsExercise1.java
+++ streams/src/test/java/part1/exercise/StreamsExercise1.java
@@ -6,6 +6,7 @@ import data.Person;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,33 +16,37 @@ public class StreamsExercise1 {
     @Test
     public void getAllEpamEmployees() {
         List<Employee> employees = Arrays.asList(
-            new Employee(new Person("John", "Galt", 20),
-                    Arrays.asList(
-                        new JobHistoryEntry(3, "dev", "epam"),
-                        new JobHistoryEntry(2, "dev", "google")
-                    )),
-            new Employee(new Person("John", "Doe", 21),
-                    Arrays.asList(
-                        new JobHistoryEntry(4, "BA", "yandex"),
-                        new JobHistoryEntry(2, "QA", "epam"),
-                        new JobHistoryEntry(2, "dev", "abc")
-                    )),
-            new Employee(new Person("John", "White", 22),
-                    Collections.singletonList(
-                        new JobHistoryEntry(6, "QA", "epam")
-                    )),
-            new Employee(new Person("John", "Galt", 23),
-                    Arrays.asList(
-                        new JobHistoryEntry(2, "dev", "google")
-        )));
+                new Employee(new Person("John", "Galt", 20),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee(new Person("John", "Doe", 21),
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "BA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee(new Person("John", "White", 22),
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        )),
+                new Employee(new Person("John", "Galt", 23),
+                        Arrays.asList(
+                                new JobHistoryEntry(2, "dev", "google")
+                        )));
 
-        List<Person> epamEmployees = null;// TODO all persons with experience in epam
+        List<Person> epamEmployees = employees.stream()
+                .filter(e -> e.getJobHistory().stream()
+                        .anyMatch(j -> j.getEmployer().equals("epam")))
+                .map(Employee::getPerson).collect(Collectors.toList());// TODO all persons with experience in epam
+
 
         assertEquals(Arrays.asList(
                 new Person("John", "Galt", 20),
                 new Person("John", "Doe", 21),
                 new Person("John", "White", 22)),
-            epamEmployees
+                epamEmployees
         );
     }
 
@@ -68,7 +73,10 @@ public class StreamsExercise1 {
                                 new JobHistoryEntry(2, "dev", "google")
                         )));
 
-        List<Person> epamEmployees = null;// TODO all persons with first experience in epam
+        List<Person> epamEmployees = employees.stream()
+                .filter(e-> !e.getJobHistory().isEmpty() && "epam".equals(e.getJobHistory().get(0).getEmployer()))
+                .map(Employee::getPerson)
+                .collect(Collectors.toList());// TODO all persons with first experience in epam
 
         assertEquals(Arrays.asList(
                 new Person("John", "Galt", 20),
@@ -80,28 +88,41 @@ public class StreamsExercise1 {
     @Test
     public void sumEpamDurations() {
         List<Employee> employees = Arrays.asList(
-            new Employee(new Person("John", "Galt", 20),
-                    Arrays.asList(
-                            new JobHistoryEntry(3, "dev", "epam"),
-                            new JobHistoryEntry(2, "dev", "google")
-                    )),
-            new Employee(new Person("John", "Doe", 21),
-                    Arrays.asList(
-                            new JobHistoryEntry(4, "BA", "yandex"),
-                            new JobHistoryEntry(2, "QA", "epam"),
-                            new JobHistoryEntry(2, "dev", "abc")
-                    )),
-            new Employee(new Person("John", "White", 22),
-                    Collections.singletonList(
-                            new JobHistoryEntry(6, "QA", "epam")
-                    )),
-            new Employee(new Person("John", "Galt", 23),
-                    Arrays.asList(
-                            new JobHistoryEntry(2, "dev", "google")
-        )));
+                new Employee(new Person("John", "Galt", 20),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee(new Person("John", "Doe", 21),
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "BA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee(new Person("John", "White", 22),
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        )),
+                new Employee(new Person("John", "Galt", 23),
+                        Arrays.asList(
+                                new JobHistoryEntry(2, "dev", "google")
+                        )));
 
 
-        int result = 0; // TODO sum
+        int result1 = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream())
+                .filter(entry -> entry.getEmployer().equals("epam"))
+                .mapToInt(JobHistoryEntry::getDuration).sum();
+        // TODO sum сколько всего лет было проведено в епаме
+        int result = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream())
+                .filter(entry -> entry.getEmployer().equals("epam"))
+                .collect(Collectors.summingInt(JobHistoryEntry::getDuration));
+        int result2 = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream())
+                .filter(entry -> entry.getEmployer().equals("epam"))
+                .reduce(0,(value,entry)->value+entry.getDuration(),Integer::sum);
+
         assertEquals(11, result);
     }
 
